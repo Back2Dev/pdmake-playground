dfac45b5391721c7a4eb39e473f62c15f07c717c
YunL <sam.looong@gmail.com>
2022-10-03 15:49:34 +1100

    Basic

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const Header = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Mainbody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mainbody = () => {
+  return (
+    <div>Mainbody</div>
+  )
+}
+
+export default Mainbody
