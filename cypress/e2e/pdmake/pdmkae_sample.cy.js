c1b3941bf28a5643ff19743b18ab8d97adbaaa52
YunL <sam.looong@gmail.com>
2022-10-10 03:57:43 +1100

    try to use cypress test codereditor (have some bug for typein)

---
+++ cypress/e2e/pdmake/pdmkae_sample.cy.js
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+
+const samples = ['basics', 'styles1', 'styles2', 'styles3', 'columns', 'tables', 'lists', 'margin', 'images', 'svgs', 'attachments'];
+
+describe('Test sample',() => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5173/')
+  })
+  
+  samples.forEach((sample) => {
+    it(`Test sample ${sample}`, () => {
+      let samplefile = cy.readFile(`./public/sample/${sample}`, 'utf8');
+      cy.window().then((win) => {
+        win.editor.setValue(samplefile);
+      });
+      // cy.get('[data-cy="codemirror"]').clear();
+      cy.get('[data-cy="updatepdfbutton"]').click({force: true})
+      cy.get('[data-cy="errorbar"]').should('not.exist')
+    });
+  });
+})
