c6830c1a0bca0f010b5bfcbee2f8eebd5a2c3955
Richa Pujara <richa.pujara20@gmail.com>
2022-10-10 13:45:15 +1100

    codefix implemented

--- src/components/code-editor/code-editor.jsx
+++ src/components/code-editor/code-editor.jsx
@@ -1,23 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
+import React, { useState } from "react"
 import { Box, ToggleButtonGroup, ToggleButton, Button, Grid } from '@mui/material';
 import { xcodeLight, xcodeDark } from '@uiw/codemirror-theme-xcode';
 import Split from 'react-split'
-import pdfMake from "pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import CodeMirror from "@uiw/react-codemirror"
+import { javascript } from "@codemirror/lang-javascript"
+import { historyField } from "@codemirror/commands"
+import pdfMake from "pdfmake/build/pdfmake"
+import pdfFonts from "pdfmake/build/vfs_fonts"
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+
 import './code-editor.css'
 import ErrorBar from "../error-bar/error-bar";
-// import { debounce } from 'lodash'
 
-import { styled } from '@mui/material/styles';
+
+const stateFields = { history: historyField }
+
+// click the button to fomat to PDF
+// const [err, setErr] = useState("")
+// const clickFormatButton = () => {
+//   if (err) {
+//     console.error(err);
+//     setErr(`Error: ${e.message}`)
+
+//   }
+//   const value = localStorage.getItem("myValue") || ""
+//   // const docDefinition = value.split('=')[1].replace(/'/g, `"`).replace(/([{,]\s*)(\S+)\s*(:)/mg, '$1"$2"$3');
+//   // const docDefinition = JSON.stringify(value.replace("var dd = ", ''), null, "\t");
+//   let dd = {}
+//   console.log(value)
+//   const docDefinition = eval(value)
+
+//   // console.log("docDefinition: ", docDefinition)
+//   const pdfDocGenerator = pdfMake.createPdf(docDefinition)
+//   pdfDocGenerator.getDataUrl((dataUrl) => {
+//     const targetElement = document.getElementById("pdfView")
+//     targetElement.src = dataUrl
+//   })
+// }
 
 const CodeEditor = () => {
-  
-  const extentions = [javascript({ jsx: true })];
+  const serializedState = localStorage.getItem("myEditorState")
+  const value = localStorage.getItem("myValue") || ""
+
+  const [err, setErr] = useState("")
   const [theme, setTheme] = useState(xcodeDark);
-  const [err,setErr] = useState("")
   const selectTheme = (event) => {
     if (event.target.value === 'dark') {
       setTheme(xcodeDark);
@@ -26,87 +53,95 @@ const CodeEditor = () => {
       setTheme(xcodeLight);
     }
   }
-  const convertDoc = (value) => {
+  const clickFormatButton = () => {
     try {
-      const newDoc = value.split('=')[1].trim();
-      const keyFinderRegEX = /([{,]\s*)(\S+)\s*(:)/mg;
-      const convertedJSONString = newDoc.replace(/\[/g,"[ ").replace(/\{/g,"{ ").replaceAll("'", "\"").replace(keyFinderRegEX, '$1"$2"$3');
-      const parsedObj = JSON.parse(convertedJSONString);
-      localStorage.setItem('myValue', JSON.stringify(parsedObj))
-      
-      return parsedObj;
-    } catch (e) { 
-      // console.log("parsing error:", e)
+      const value = localStorage.getItem("myValue") || ""
+      // const docDefinition = value.split('=')[1].replace(/'/g, `"`).replace(/([{,]\s*)(\S+)\s*(:)/mg, '$1"$2"$3');
+      // const docDefinition = JSON.stringify(value.replace("var dd = ", ''), null, "\t");
+      let dd = {}
+      // console.log(value)
+      const docDefinition = eval(value)
+
+      // console.log("docDefinition: ", docDefinition)
+      const pdfDocGenerator = pdfMake.createPdf(docDefinition)
+      pdfDocGenerator.getDataUrl((dataUrl) => {
+        const targetElement = document.getElementById("pdfView")
+        targetElement.src = dataUrl
+      })
+    } catch (e) {
       setErr(`Error: ${e.message}`)
-    }
-  }
-  const [text, setText] = useState("var dd = {content: ['Hello world', 'Hello World!']}")
-    
-  const [doc, setDoc] = useState(convertDoc(text))
-  
-  const pdfConverter = (doc) => {
-    if (doc) {
-      try {
-        const pdfDocGenerator = pdfMake.createPdf(doc);
-        pdfDocGenerator.getDataUrl((dataUrl) => {
-          const iframe = document.querySelector('#pdf-viewer');
-          iframe.src = dataUrl;
-        }
-        );
-      } catch (e) {
-        console.log("pdf rendering error:", e)
-        setErr(`error:${e.message}`)
-      }
+      setTimeout(() => {
+        setErr("")
+      }, 3000)
     }
   }
 
-  const [pdfData, setPdfData] = useState(pdfConverter(doc))
- 
-  const handleInputChange = (value) => {
-    // setText(value);
-    if (err) {
-      setErr("")
-    }
-    setDoc(convertDoc(value));
-  }
-  
   return (
-    <Grid container columns={12} className="main-area">
-       <Box width="100vw" >
-         <Split className="split">
-          <Grid item columns={1}>
-            <Box sx={{ bgcolor: '#2a313e',height:'10vh', color: '#ffffff' }} >
-              <CodeMirror
-                value={text}
-                extensions={extentions}
-                width='auto'
-                color='#2A313E'
-                placeholder="your code needs to be in var dd = {content: ''} format"
-                basicSetup={{
-                  allowMultipleSelections: false,
-                  indentOnInput: true
-                }}
-                onChange={handleInputChange}
-                theme={theme}
-              />
-              <ErrorBar errorMessage={err} />
-            </Box > 
-          </Grid>
-          <Grid item columns={1}>
-            <Box sx={{ bgcolor: '#cccccc', height: '80vh', color: '#FFFFFF' }} id="iframeContainer">
-              <iframe id="pdf-viewer" />
-            </Box >
-            <div>
-              <Button onClick={handleInputChange}>Update PDF</Button>
-              <ToggleButtonGroup exclusive={true} className='MuiToggleButtonGroup-groupedHorizontal theme-selector'>
-                <ToggleButton value="dark" onClick={selectTheme} aria-label="Dark-theme">Dark</ToggleButton>
-                <ToggleButton value="light" onClick={selectTheme} aria-label="Light-theme">Light</ToggleButton>
-              </ToggleButtonGroup>
-            </div>
-          </Grid>
-        </Split>
-       </Box>
-     </Grid>
+    <>
+      <Grid container columns={12} className="main-area">
+        <Box width="100vw" >
+          <Split className="split">
+            <Grid item columns={1}>
+              <Box sx={{ bgcolor: '#2a313e', height: '10vh', color: '#ffffff' }} >
+                <CodeMirror
+                  value={value}
+                  height="80vh"
+                  initialState={
+                    serializedState
+                      ? {
+                        json: JSON.parse(serializedState || ""),
+                        fields: stateFields,
+                      }
+                      : undefined
+                  }
+                  onChange={(value, viewUpdate) => {
+                    // clickFormatButton();
+                    // console.log(
+                    //   value
+                    //     .replace("var dd = ", "")
+                    //     .replace(/'/g, `"`)
+                    //     .replace(/\[/g, "[ ")
+                    //     .replace(/\{/g, "{ ")
+                    //     .replace(/([{,]\s*)(\S+)\s*(:)/gm, '$1"$2"$3')
+                    // )
+                    localStorage.setItem("myValue", value.replace("var dd", 'dd'))
+                    const state = viewUpdate.state.toJSON(stateFields)
+                    localStorage.setItem("myEditorState", JSON.stringify(state))
+                  }}
+                  extensions={[javascript({ jsx: true })]}
+                  basicSetup={{
+                    dropCursor: false,
+                    allowMultipleSelections: false,
+                    indentOnInput: false,
+                    lintKeymap: true,
+                  }}
+                  theme={theme}
+                />
+                <ErrorBar errorMessage={err} />
+              </Box >
+            </Grid>
+            <Grid item columns={1}>
+              <Box sx={{ bgcolor: '#cccccc', height: '80vh', color: '#FFFFFF' }} id="iframeContainer">
+                <iframe
+                  id="pdfView"
+                  src=""
+                  width="100%"
+                  height="100%"
+                  border="0"
+                ></iframe>
+              </Box >
+              <div>
+                <Button onClick={clickFormatButton}>Update PDF</Button>
+                <ToggleButtonGroup exclusive={true} className='MuiToggleButtonGroup-groupedHorizontal theme-selector'>
+                  <ToggleButton value="dark" onClick={selectTheme} aria-label="Dark-theme">Dark</ToggleButton>
+                  <ToggleButton value="light" onClick={selectTheme} aria-label="Light-theme">Light</ToggleButton>
+                </ToggleButtonGroup>
+              </div>
+            </Grid>
+          </Split>
+        </Box>
+      </Grid>
+    </>
   )
 }
 
